c7719c21cc578ed07a44510ad6b9f44836ea35c1
Hans Doller <kryo2k@gmail.com>
2014-02-19 00:59:18 -0600

    Initial commit of project contents
    I'll probably fudge something up here.

--- README.md
+++ README.md
@@ -1,4 +1,64 @@
 connect-mongoose
 ================
 
-Port of kcbanner's Connect-Mongo (MongoStore) to use mongoose models for storing sessions
+Port of kcbanner's Connect-Mongo (MongoStore) to use mongoose models for storing sessions.
+
+# Installation
+```
+npm install connect-mongoose
+```
+
+# Quick example
+**In a real setup, your layout will probably be different.**
+
+```JavaScript
+var // node requirements:
+express = require('express'),
+mongoose = require('mongoose'),
+connectMongoose = require('connect-mongoose');
+
+var // create a minimalistic schema for storing sessions data:
+Schema = mongoose.Schema,
+Session = mongoose.model('Session', new Schema({
+  sid: String,
+  data: String,
+  lastActivity: {
+    type: Date,
+    expires: 900 // removed automatically after 15 minutes of no activity <3 mongo.
+  }
+}));
+
+var
+// create a connectMongoose store prototype which is linked on the mongoose model:
+mongooseStore = connectMongoose(express, Session),
+
+// connect to your database
+db = mongoose.connect('mongodb://localhost/test'),
+
+// create our express application instance:
+app = express();
+
+// configure express session with our session store:
+app.use(express.session(
+  secret: 'your own secret here',
+  store: new mongooseStore({
+    clear_interval: 900 // every 15 minutes
+  }),
+  cookie: {
+    maxAge: 1209600 * 1000 // 2 weeks
+  }
+));
+```
+
+# Configuration
+The mongooseStore prototype accepts the following options, in addition to those
+inherited from the connect memory store:
+
+key                       | default       | description
+--------------------------|---------------|---------------------------------------------------------
+propertySessionId         | sid           | Model property that holds session id (i.e. PADiGWwMH2Ym2iKuMQVV6SPM) not ObjectId (that's _id)
+propertyData              | data          | Model property that holds the session data
+propertyLastActivity      | lastActivity  | Model property that remembers last activity of this session (this is updated on session.set())
+updateLastActivity        | true          | If the session's lastActivity should be on set()
+defaultExpirationTime     | 86400000      | Default expiration time for the cookie. (default is 24hrs)
+
---
+++ index.js
@@ -0,0 +1,150 @@
+'use strict';
+
+/**
+ * Express Session Store for MongooseJS
+ * Partially adapted from connect-mongo's implmentation.
+ *
+ * @author Hans Doller <kryo2k@gmail.com>
+ */
+
+var
+_ = require('lodash'),
+util = require('util');
+
+var
+defaults = {
+
+  // properties which should exist in model:
+  propertySessionId:    'sid',             // holds session id (i.e. PADiGWwMH2Ym2iKuMQVV6SPM) not ObjectId (that's _id)
+  propertyData:         'data',            // holds the session data
+  propertyLastActivity: 'lastActivity',    // last activity of this session
+
+  // if last activity should be updated automatically on get()
+  updateLastActivity: true,
+
+  // default expiration time for sessions
+  defaultExpirationTime:  86400 * 1000     // 24hrs.
+};
+
+/**
+ * MongooseStore bootstrap constructor
+ *
+ * @param {Express} express         Instance of Express/Connect
+ * @param {Model}   mongooseModel   Model to use for sessions
+ * @return {Function}
+ */
+function MongooseStore(express, mongooseModel) {
+  var
+  self = this, Store = express.session.Store;
+
+  if(!express || !express.session) {
+    throw 'Express was not passed in constructor.';
+  }
+
+  if(!mongooseModel || !mongooseModel.findById) {
+    throw 'Mongoose Model was not passed in constructor.';
+  }
+
+  /**
+   * MongooseStore session store constructor
+   */
+  function MStore(config) {
+    var
+    me = this,
+
+    // define our options for this store instance
+    opts = _.extend({}, defaults, config),
+
+    // default (un)serializer using JSON
+    serializer = JSON.stringify,
+    unserializer = JSON.parse;
+
+    // call superclass method
+    Store.call(me, opts);
+
+    var
+    defExpireTime  = opts.defaultExpirationTime,
+    propSid        = opts.propertySessionId,
+    propData       = opts.propertyData,
+    propLastActive = opts.propertyLastActivity;
+
+    // creates/updates a query to search for a certain property
+    function _applyProp(property, value, obj) {
+      obj = obj || {};
+      obj[property] = value;
+      return obj;
+    }
+
+    // checks a session document's expiration date to see if it's still valid
+    function _checkExpiration(session) {
+      return !session[propLastActive] || new Date < (session[propLastActive].getTime() + defExpireTime);
+    }
+
+    _.extend(me, {
+
+      // get session data by session id:
+      get: function (sid, callback) {
+        var cb = callback || _.noop;
+
+        // ensureIndex is already called on start-up
+        mongooseModel.findOne(_applyProp(propSid, sid), function(err, doc) {
+
+          if(err || !doc) { // exit immediately
+            cb(err, null);
+            return;
+          }
+
+          if(_checkExpiration(doc) && doc[propData] !== undefined) { // we're still valid:
+            cb(null, unserializer(doc[propData]));
+          }
+          else { // needs clean-up
+            me.destroy(sid, cb);
+          }
+        });
+      },
+
+      // set session data by session id:
+      set: function (sid, session, callback) {
+        var rec = {};
+
+        // apply our properties:
+        _applyProp(propSid, sid, rec);
+        _applyProp(propData, serializer(session), rec);
+
+        // opportunity to do:
+        _applyProp(propLastActive, new Date(), rec);
+
+        mongooseModel.findOneAndUpdate(_applyProp(propSid, sid), rec, {upsert: true}, function(err){
+          (callback||_.noop)(err);
+        });
+      },
+
+      // destroy session by session id:
+      destroy: function (sid, callback) {
+        mongooseModel.findOneAndRemove(_applyProp(propSid, sid), function(err) {
+          (callback||_.noop)(err);
+        });
+      },
+
+      // get the number of total sessions:
+      length: function (callback) {
+        mongooseModel.count({}, callback||_.noop);
+      },
+
+      // clear all stored sessions:
+      clear: function (callback) {
+        mongooseModel.remove({}, callback||_.noop);
+      }
+    });
+
+    return me;
+  }
+
+  // inherit by default from connect memory store:
+  util.inherits(MStore, Store);
+
+  // return preconfigured mongoose store:
+  return MStore;
+}
+
+module.exports = MongooseStore;
